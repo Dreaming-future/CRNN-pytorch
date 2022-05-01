3b51bcb274e462bd6e8624b508859ddfb2536c85
Dreaming-future <1016617094@qq.com>
2022-05-01 22:52:19 +0800

    delete

--- summary.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torchinfo import summary
-from crnn import CRNN
-
-if __name__ == "__main__":
-    # 需要使用device来指定网络在GPU还是CPU运行
-    device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    crnn = CRNN(3,10).to(device)
-    summary(crnn,(1,3,32,128))
